aac07c2620b29b9a05b8d117e0a28dc965336698
Rohan Kapur <me@rohankapur.com>
2018-06-18 14:41:00 -0700

    ss

--- summer.php
+++ summer.php
@@ -1,55 +1,59 @@
-<h1>Summer navigator</h1>
-<p>Goals:</p>
-<ol>
-<li>“Sort myself out” — Peterson style. Take inspiration from Nick. This means being more disciplined, do tasks I don’t feel like doing just because they’re menial, more motivated, doing things that I set out for myself, less fooling around / bumming around, more upfront to reality with myself. Live more independently.</li>
-<li>Get on track for O-1 visa. Keep on track. Go for it.</li>
-<li>Keep challenging myself re. socially and re. girls. Use therapy as help where the help is needed.</li>
-<li>Realize certain dreams that I have, where I have the capacity to, in SF. This is especially relevant towards music.</li>
-<li>Pursue excellence in whatever I’m working towards, where relevant. Don’t have tunnel vision towards one goal eg. “visa” or “green card”. Relevant to work eg. like Alex’s. If I built a really cool influencer solution, this optimizes for serendipity.</li>
-<li>Create, learn, have fun.</li>
-<li>Be more conservative with my money! Much more conservative.</li>
-</ol>
-<p>As related to:</p>
-<ul>
-<li>exploring / camping / hiking / biking / traveling</li>
-<li>skating</li>
-<li>punk rock (listening, performing, making music)</li>
-<li>building things / extended projects</li>
-<li>jobs and work</li>
-<li>relationships</li>
-<li>learning: reading books, news, articles, etc.</li>
-<li>having good conversations with cool people (want to make this more of a priority this quarter)</li>
-<li>reaching out to more ambitious people</li>
-<li>driving</li>
-</ul>
-<p>Macro milestones:</p>
-<ul>
-<li>apply for O-1 with Ben</li>
-<li>join punk community: get better at guitar, learn new instruments, play a gig, make covers, start writing original music?</li>
-<li>raise funding w Alex for AdParty</li>
-<li>get better at skating… probably learn how to commit, and learn how to bail/fall well</li>
-<li>get a girlfriend</li>
-<li>complete my summer reading list</li>
-<li>pursue projects with Nick (CryptoBets, Medium article, business ideas, )</li>
-<li>get license</li>
-<li>work with Bill on skills platform</li>
-<li>get good at cooking!</li>
-<li>explore SF, get acquainted with the city</li>
-</ul>
-<p>Micro milestones:</p>
-<ul>
-<li>evaluate this list everyday</li>
-<li>check news everyday</li>
-<li>read everyday</li>
-<li>create everyday</li>
-<li>get out everyday</li>
-<li>minimize Things list</li>
-<li>set new tasks based on macro milestones + “as related to” section</li>
-</ul>
-<p>Productivity workflow:</p>
-<ul>
-<li>Use Things to add tasks</li>
-<li>Set task for certain day, make sure to do it on that day — force myself!</li>
-<li>Don’t crowd “Today”</li>
-<li>Use projects for sub-tasks</li>
-</ul>
+<body style="font-family: 'Avenir Next'">
+
+	<h1>Summer navigator</h1>
+	<p>Goals:</p>
+	<ol>
+	<li>“Sort myself out” — Peterson style. Take inspiration from Nick. This means being more disciplined, do tasks I don’t feel like doing just because they’re menial, more motivated, doing things that I set out for myself, less fooling around / bumming around, more upfront to reality with myself. Live more independently.</li>
+	<li>Get on track for O-1 visa. Keep on track. Go for it.</li>
+	<li>Keep challenging myself re. socially and re. girls. Use therapy as help where the help is needed.</li>
+	<li>Realize certain dreams that I have, where I have the capacity to, in SF. This is especially relevant towards music.</li>
+	<li>Pursue excellence in whatever I’m working towards, where relevant. Don’t have tunnel vision towards one goal eg. “visa” or “green card”. Relevant to work eg. like Alex’s. If I built a really cool influencer solution, this optimizes for serendipity.</li>
+	<li>Create, learn, have fun.</li>
+	<li>Be more conservative with my money! Much more conservative.</li>
+	</ol>
+	<p>As related to:</p>
+	<ul>
+	<li>exploring / camping / hiking / biking / traveling</li>
+	<li>skating</li>
+	<li>punk rock (listening, performing, making music)</li>
+	<li>building things / extended projects</li>
+	<li>jobs and work</li>
+	<li>relationships</li>
+	<li>learning: reading books, news, articles, etc.</li>
+	<li>having good conversations with cool people (want to make this more of a priority this quarter)</li>
+	<li>reaching out to more ambitious people</li>
+	<li>driving</li>
+	</ul>
+	<p>Macro milestones:</p>
+	<ul>
+	<li>apply for O-1 with Ben</li>
+	<li>join punk community: get better at guitar, learn new instruments, play a gig, make covers, start writing original music?</li>
+	<li>raise funding w Alex for AdParty</li>
+	<li>get better at skating… probably learn how to commit, and learn how to bail/fall well</li>
+	<li>get a girlfriend</li>
+	<li>complete my summer reading list</li>
+	<li>pursue projects with Nick (CryptoBets, Medium article, business ideas, )</li>
+	<li>get license</li>
+	<li>work with Bill on skills platform</li>
+	<li>get good at cooking!</li>
+	<li>explore SF, get acquainted with the city</li>
+	</ul>
+	<p>Micro milestones:</p>
+	<ul>
+	<li>evaluate this list everyday</li>
+	<li>check news everyday</li>
+	<li>read everyday</li>
+	<li>create everyday</li>
+	<li>get out everyday</li>
+	<li>minimize Things list</li>
+	<li>set new tasks based on macro milestones + “as related to” section</li>
+	</ul>
+	<p>Productivity workflow:</p>
+	<ul>
+	<li>Use Things to add tasks</li>
+	<li>Set task for certain day, make sure to do it on that day — force myself!</li>
+	<li>Don’t crowd “Today”</li>
+	<li>Use projects for sub-tasks</li>
+	</ul>
+
+</body>
