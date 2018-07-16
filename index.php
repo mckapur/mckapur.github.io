411c7b8c89fab4f45d33fdf5359f952bb530b781
Rohan Kapur <me@rohankapur.com>
2018-07-16 06:59:44 -0700

    sss

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
 				  <li>did cool, valuable things  👍
 					  <ul>
 					  	<li>coded for <a href="http://grab.com">Grab</a> (2016), <a href="https://www.crunchbase.com/organization/eagerpanda">EagerPanda</a> (2015), and <a href="https://www.singpost.com/">Singapore Post</a> (2014)</li>
-					  	<li>started a dev community in my high school, incl. <a href="https://www.youtube.com/watch?v=l4rslNqCbsY">IDEA Hacks</a>: a youth-centric hackathon</li>
+					  	<li>started a dev community in my high school, including <a href="https://www.youtube.com/watch?v=l4rslNqCbsY">IDEA Hacks</a>: a youth-centric all age hackathon</li>
 					  	<li><a href="https://drive.google.com/file/d/0B_tD-Ps4EVdDNDkwSTdiWm1wWm8/view?usp=sharing">performed</a> "Jesus of Suburbia" by Green Day in front of 400+ people  🎸</li>
 					  	<li><a href="https://imgur.com/a/3koMH">lost ~60 lbs</a> on the Keto diet from Feb - Sep 2017  🍖</li>
 				  	</ul>
