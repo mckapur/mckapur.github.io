119eca763ff0a88061388104673d9581debb0369
Rohan Kapur <me@rohankapur.com>
2018-11-29 00:04:49 -0800

    suh

--- index.php
+++ index.php
@@ -120,6 +120,7 @@
 			<p class="header">_I LAST READ_ 📙</p>
 			<div class="textbody" id="reading">
 					<br>
+					<li>Punk Rock: An Oral History &nbsp;💯</li>
 				  <li>Encountering Naturalism: A Worldview and Its Uses &nbsp;👍👍👍</li>
 				  <li>In Defense of a Liberal Education  &nbsp;👍👍</li>
 				  <li>The End of Education  &nbsp;👍👍</li>
