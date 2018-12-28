b90da31044dcf03840869419fd201fb36f82252c
mckapur <me@rohankapur.com>
2018-12-28 23:00:35 +0800

    la

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
 			<div class="textbody">
 				<ul>
 				  <li>working on punk house.<sup>coming soon</sup> 🚀</li>
-				  <li>studying liberal arts at Stanford University 🎄<br> (American Studies: focusing on individualism, counterculture, anarchism, and more)</li>
+				  <li>studying liberal arts at Stanford University 🎄<br> (American Studies & maybe Sociology: critical theory, individualism, counterculture, anarchism, and more)</li>
 				  <li>working on my first EP  &nbsp;🎶</li>
 				</ul>
 			</div>
