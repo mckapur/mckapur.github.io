abd49b00a9f604f8d00a40d4ce5b65a1ef8a874e
Rohan Kapur <wheresthemoney680@gmail.com>
2019-07-09 11:08:51 +0800

    la

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 			<div class="textbody">
 				<ul>
 				  <li>🚀 <a href="https://www.punkhouse.co">Punk House.</a></li>
-          <li>🎶 <a href="https://www.instagram.com/fullvolumeonly/">Full Volume Only</a></li>
+          <li>🎶 <a href="https://open.spotify.com/artist/7z8S1uCUgYBYX2lTWx9udV?si=n4JVqhxEQDuVgR1jw4MUbQ">Full Volume Only</a></li>
 				  <li>🎄 Stanford University<br></li>
 				</ul>
 			</div>
