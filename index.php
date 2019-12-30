d5eecdb565806f1a59cf98185ce099528d4ce7a9
Rohan Kapur <me@rohankapur.com>
2019-12-30 12:58:14 -0800

    fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="index.css" media="screen"/>
-		<title>Rohan Kapur</title>
+		<title>rohan kapur</title>
 		<meta charset="UTF-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 
