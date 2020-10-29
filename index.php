7efcbd617d9c745b3f47e2869502bb348dae0058
Rohan Kapur <me@rohankapur.com>
2020-10-29 16:20:43 -0700

    fix

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
 			<div class="textbody">
 			  	<ul>
 						<li><a class="tag" href="https://playbackbone.com">Backbone</a> — next level play, now in your hands — with the kickass team :)</li>
-	          <li><a href="https://i.imgur.com/m64vOLK.jpg">a little DIY music studio in my room</a> & <a href="https://imgur.com/5AK3T7j">a skate pedal board</a> :)</li>
+	          <li><a href="https://i.imgur.com/m64vOLK.jpg">a little DIY music studio in my old room</a> & <a href="https://imgur.com/5AK3T7j">a skate pedal board</a> :)</li>
             <li><a href="https://stanfordguide.org/the-ultimate-guide-to-the-e-2-visa-for-international-student-founders-6725a5480d4e">The Ultimate Guide To The E-2 Visa For International Student Founders</a></li>
             <li><a href="http://www.fullvolumeonly.com">Full Volume Only</a>: young san francisco noisemakers. playing shows and making records. 🤘🏻🎸</li>
 			  		<li><a class="tag" href="https://winno.app">Winno</a>: 21st Century News, built with <a class="tag" href="https://nickrub.in">Nick</a></li>
